574bf461b42e5d100d8dd2110ea7985e63d359b7
Thiago <thiagodepaulo@gmail.com>
2020-10-07 16:46:20 -0300

    transductive update 3

--- src/exp/test_transd_1.py
+++ src/exp/test_transd_1.py
@@ -5,23 +5,25 @@ Created on Thu Jan 18 19:57:32 2018
 
 @author: thiagodepaulo
 """
-import sys
-sys.path.append("..")
-import logging
-import numpy as np
-from tpbg import TPBG
-from util import Loader, SimplePreprocessing
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.datasets import fetch_20newsgroups
-from sklearn.decomposition import NMF, LatentDirichletAllocation
 from sklearn.metrics import f1_score, confusion_matrix, classification_report
+from sklearn.decomposition import NMF, LatentDirichletAllocation
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.pipeline import Pipeline
+from ..util import Loader, SimplePreprocessing
+from ..tpbg import TPBG
+import numpy as np
+import logging
+import sys
+
 
 #categories = ['alt.atheism', 'talk.religion.misc', 'comp.graphics', 'sci.space']
-newsgroups_train = fetch_20newsgroups(subset='train', remove=('headers','footers','quotes'))
-                                       #, categories=categories)
-newsgroups_test = fetch_20newsgroups(subset='test', remove=('headers','footers','quotes'))
-                                       #, categories=categories)
+newsgroups_train = fetch_20newsgroups(
+    subset='train', remove=('headers', 'footers', 'quotes'))
+# , categories=categories)
+newsgroups_test = fetch_20newsgroups(
+    subset='test', remove=('headers', 'footers', 'quotes'))
+# , categories=categories)
 
 n_train = len(newsgroups_train.data)
 n_test = len(newsgroups_test.data)
@@ -32,10 +34,11 @@ all_data = newsgroups_train.data + newsgroups_test.data
 indices = np.arange(n)
 train_target = newsgroups_train.target
 test_target = newsgroups_test.target
-all_target = np.concatenate((newsgroups_train.target,newsgroups_test.target),axis=None)
-selected_classes = [0,1,5]
-semi_target = [ x if x in selected_classes else -1 for x in train_target]
-semi_target = np.array(semi_target + [ -1 ] * n_test)
+all_target = np.concatenate(
+    (newsgroups_train.target, newsgroups_test.target), axis=None)
+selected_classes = [0, 1, 5]
+semi_target = [x if x in selected_classes else -1 for x in train_target]
+semi_target = np.array(semi_target + [-1] * n_test)
 unlabeled_set = np.argwhere(semi_target == -1).ravel()
 
 
@@ -49,27 +52,30 @@ M = vectorizer.fit_transform(M)
 
 print('rodando TPBG...')
 
-K=20
+K = 20
 pbg = TPBG(K, alpha=0.005, beta=0.001, local_max_itr=50, global_max_itr=10,
-            local_threshold = 1e-6, global_threshold = 1e-6, feature_names=vectorizer.get_feature_names())
+           local_threshold=1e-6, global_threshold=1e-6, feature_names=vectorizer.get_feature_names())
 pbg.fit(M, semi_target)
 model = pbg
 
 #true_labels = all_target[unlabeled_set]
-true_labels = [ l if l in selected_classes else -1 for l in all_target[unlabeled_set]]
+true_labels = [l if l in selected_classes else -
+               1 for l in all_target[unlabeled_set]]
 predicted_labels = pbg.transduction_[unlabeled_set]
 
-labels_names = ['unknown'] + [ newsgroups_train.target_names[cls] for cls in sorted(selected_classes)]
+labels_names = ['unknown'] + [newsgroups_train.target_names[cls]
+                              for cls in sorted(selected_classes)]
 labels_id = sorted(set(true_labels))
 
 print(len(unlabeled_set))
 print(set(true_labels))
 print(set(predicted_labels))
 cm = confusion_matrix(true_labels, predicted_labels)
-print(classification_report(true_labels, predicted_labels, labels=labels_id, target_names=labels_names))
+print(classification_report(true_labels, predicted_labels,
+                            labels=labels_id, target_names=labels_names))
 print("Confusion matrix")
 print(cm)
 
 print(n_train, n_test)
-pbg.print_top_topics(labels_dict=dict(zip(labels_id,labels_names)))
+pbg.print_top_topics(labels_dict=dict(zip(labels_id, labels_names)))
 print('fim.')
--- src/tpbg.py
+++ src/tpbg.py
@@ -11,8 +11,8 @@ from sklearn.utils.multiclass import unique_labels
 class TPBG(BaseEstimator, ClassifierMixin):
 
     def __init__(self, n_components, alpha=0.05, beta=0.0001, local_max_itr=50,
-              global_max_itr=50, local_threshold = 1e-6, global_threshold = 1e-6,
-              save_interval=-1, is_semisupervised=True, feature_names=None):
+                 global_max_itr=50, local_threshold=1e-6, global_threshold=1e-6,
+                 save_interval=-1, is_semisupervised=True, feature_names=None):
         self.n_components = n_components
         self.alpha = alpha
         self.beta = beta
@@ -23,13 +23,13 @@ class TPBG(BaseEstimator, ClassifierMixin):
         self.is_semisupervised = is_semisupervised
         self.feature_names = feature_names
         self.is_fitted_ = False
-        self.map_class_ = {-1:-1} # key=class, value=index position
-        self.free_id = set(range(n_components)) # list of index position
+        self.map_class_ = {-1: -1}  # key=class, value=index position
+        self.free_id = set(range(n_components))  # list of index position
 
     def local_propag(self, j):
         local_niter = 0
         words = [x for x in self.X[j].nonzero()[1]]
-        log_F = np.log(self.X[j,words].toarray().T)
+        log_F = np.log(self.X[j, words].toarray().T)
         log_Aj = self.log_A[j]
         log_Bw = self.log_B[words]
         while local_niter < self.local_max_itr:
@@ -37,7 +37,7 @@ class TPBG(BaseEstimator, ClassifierMixin):
             oldA_j = log_Aj
             log_C = log_Aj + log_Bw
             log_C = log_C - logsumexp(log_C, axis=1, keepdims=True)
-            log_Aj = np.log(self.alpha + np.sum(np.exp(log_F + log_C),axis=0))
+            log_Aj = np.log(self.alpha + np.sum(np.exp(log_F + log_C), axis=0))
             mean_change = np.mean(abs(log_Aj - oldA_j))
             if mean_change <= self.local_threshold:
                 #print('convergiu itr %s' %local_niter)
@@ -46,17 +46,19 @@ class TPBG(BaseEstimator, ClassifierMixin):
 
     def global_propag(self):
         for i in tqdm(range(self.nwords), ascii=True, desc='global propagation:   '):
-            docs = [d for d in self.Xc[:,i].nonzero()[0]]
+            docs = [d for d in self.Xc[:, i].nonzero()[0]]
             log_F = np.log(self.X[docs, i].toarray())
             log_C = self.log_A[docs] + self.log_B[i]
             log_C = log_C - logsumexp(log_C, axis=1, keepdims=True)
-            self.log_B[i] = np.log(np.sum(np.exp(log_F + log_C),axis=0))
+            self.log_B[i] = np.log(np.sum(np.exp(log_F + log_C), axis=0))
         self.log_B = self.log_B - logsumexp(self.log_B, axis=0)
         self.log_B = np.log(self.beta + np.exp(self.log_B))
 
     def _init_matrices(self):
-        self.log_A = np.log(np.random.dirichlet(np.ones(self.n_components), self.ndocs))
-        self.log_B = np.log(np.random.dirichlet(np.ones(self.nwords), self.n_components).transpose())
+        self.log_A = np.log(np.random.dirichlet(
+            np.ones(self.n_components), self.ndocs))
+        self.log_B = np.log(np.random.dirichlet(
+            np.ones(self.nwords), self.n_components).transpose())
 
     def set_class(self, cls_id, pos_id):
         self.free_id.remove(pos_id)
@@ -74,22 +76,23 @@ class TPBG(BaseEstimator, ClassifierMixin):
             self._init_matrices()
             # create map of
             for cls_id in np.unique(y):
-                self.map_class_.setdefault(cls_id, self.free_id.pop() )
+                self.map_class_.setdefault(cls_id, self.free_id.pop())
         self.bgp()
         self.components_ = np.exp(self.log_B.T)
         self.is_fitted_ = True
 
         # set the transduction item
-        self.inv_map_class_ = { v:k for k,v in self.map_class_.items() } # key=idx, value=class_id
-        self.transduction_ = np.array([self.inv_map_class_.get(idx,-1)
-                                    for idx in np.argmax(self.log_A, axis=1)])
+        # key=idx, value=class_id
+        self.inv_map_class_ = {v: k for k, v in self.map_class_.items()}
+        self.transduction_ = np.array([self.inv_map_class_.get(idx, -1)
+                                       for idx in np.argmax(self.log_A, axis=1)])
 
         return self
 
     def transform(self, X):
         if not self.X != X:
             return np.exp(self.log_A)
-        log_A_back, X_back, local_max_itr_back  = self.log_A, self.X, self.local_max_itr
+        log_A_back, X_back, local_max_itr_back = self.log_A, self.X, self.local_max_itr
         ndocs = X.shape[0]
         self.log_A = np.ones((ndocs, self.n_components))
         self.X = X
@@ -105,11 +108,11 @@ class TPBG(BaseEstimator, ClassifierMixin):
         check_is_fitted(self, 'is_fitted_')
         D = self.transform(X)
         nclass = len(self.map_class_)-1
-        return np.array([self.inv_map_class_.get(idx,-1) for idx in np.argmax(D, axis=1)])
+        return np.array([self.inv_map_class_.get(idx, -1) for idx in np.argmax(D, axis=1)])
 
     def bgp(self, labelled=None):
         global_niter = 0
-        while global_niter < self.global_max_itr :
+        while global_niter < self.global_max_itr:
             self.max = 1
             for j in tqdm(range(self.ndocs), ascii=True, desc=f'docs processed (itr {global_niter})'):
                 self.local_propag(j)
@@ -117,32 +120,31 @@ class TPBG(BaseEstimator, ClassifierMixin):
                     self.supress2(j)
             self.global_propag()
             global_niter += 1
-            #self.print_top_topics()
+            # self.print_top_topics()
 
-    def supress2(self,j):
+    def supress2(self, j):
         #cls = self.classes_[self.y[j]]
         # class id to index position
         pos_id = self.map_class_[self.y[j]]
         self.log_A[j].fill(np.log(self.alpha))
         self.log_A[j][pos_id] = np.max(self.log_A)
 
-    def supress(self,j):
+    def supress(self, j):
         aux = self.log_A[j][self.y[j]]
         self.log_A[j].fill(np.log(self.alpha))
         self.log_A[j][self.y[j]] = aux
-
 
     def print_top_topics(self, n_top_words=10, labels_dict=None):
         if self.feature_names == None:
             return
         for k, topic in enumerate(self.log_B.transpose()):
-            l = [self.feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]]
-            if labels_dict is None or k not in labels_dict:
+            l = [self.feature_names[i]
+                 for i in topic.argsort()[:-n_top_words - 1:-1]]
+            cls_id = self.inv_map_class_.get(k, -1)
+            if labels_dict is None:
                 print(f'topic {k}: ' + ', '.join(l))
             else:
-                cls_id = self.inv_map_class_[k]
                 print(f'topic {k} [{labels_dict[cls_id]}]: ' + ', '.join(l))
 
 
-#class ZPBG(TPBG):
- 
+# class ZPBG(TPBG):
